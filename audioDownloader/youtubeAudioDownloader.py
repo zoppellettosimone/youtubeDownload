3ed2a278405739966dcae819b988f070ea65b245
simonezoppelletto <zoppellettosimone@gmail.com>
2022-03-30 22:17:25 +0200

    Update

--- audioDownloader/youtubeAudioDownloader.py
+++ audioDownloader/youtubeAudioDownloader.py
@@ -5,69 +5,81 @@ import os
 from pathlib import Path
 from moviepy.editor import *
 
+
 def buttonCommand():
     download()
     root.destroy()
 
+
 def downloadVideos(ys):
-    #Get title name of video
-        titleName = ys.title.replace('.', '').replace(',', '').replace(':', '').replace('?', '')
+    # Get title name of video
+    titleName = ys.title.replace('.', '').replace(',', '').replace(
+        ':', '').replace('?', '').replace('"', '').replace('/', '').replace("\\", '')
 
-        #Add .mp4 to tile name
-        newTitleName = titleName + ".mp4"
+    # Add .mp4 to tile name
+    newTitleName = titleName + ".mp4"
 
-        print("Download Audio Start")
+    print("Download Audio Start")
 
-        #Download it in Downloads\Videos
-        ys.download()
+    # Download it in Downloads\Videos
+    ys.download()
 
-        #Trasform mp4 file into mp3 file
-        videoclip = VideoFileClip(newTitleName)
-        audioclip = videoclip.audio
-        audioclip.write_audiofile(titleName + '.mp3')
-        audioclip.close()
-        videoclip.close()
+    # Trasform mp4 file into mp3 file
+    videoclip = VideoFileClip(newTitleName)
+    audioclip = videoclip.audio
+    audioclip.write_audiofile(titleName + '.mp3')
+    audioclip.close()
+    videoclip.close()
 
-        #Remove .mp4 original file
-        os.remove(newTitleName) 
+    # Remove .mp4 original file
+    os.remove(newTitleName)
 
-        print('Youtube Audio Download Complete in "Downloads\Audio"')
+    print('Youtube Audio Download Complete in "Downloads\Audio"')
+
 
 def download():
     global i
     i = i + 1
-    
+
     link = URL.get()
 
-    if(i==1):
-        #Change the current directory
+    if(i == 1):
+        # Change the current directory
         cwd = os.getcwd()
-        #cd ..
+        # cd ..
         os.chdir(cwd + '\..')
-        #Change the current directory
+        # Change the current directory
         cwd = os.getcwd()
-        #Check if there are folders "Download\Audio"
+        # Check if there are folders "Download\Audio"
         listDir = os.listdir()
         thereis = False
         for j in range(0, len(listDir), 1):
             if((cwd + '\\' + listDir[j]) == (cwd + "\Downloads")):
                 thereis = True
-        print(thereis)
-        #mkdir "Download"
+        # mkdir "Download"
         if(thereis == False):
             os.mkdir(cwd + "\Downloads")
-        #cd "Download"
+        # cd "Download"
         os.chdir(cwd + "\Downloads")
-        #Change the current directory
+        # Change the current directory
         cwd = os.getcwd()
-        #mkdir "Audio"
+        # if "Downloads" also "Audio" not exist
         if(thereis == False):
+            # mkdir "Audio"
             os.mkdir(cwd + "\Audio")
-        #cd "Audio"
+        elif (thereis == True):
+            #if "Downloads" exist, check if "Audio" exist or not
+            thereis = False
+            for j in range(0, len(listDir), 1):
+                if((cwd + '\\' + listDir[j]) == (cwd + "\Audio")):
+                    thereis = True
+            if(thereis == False):
+                os.mkdir(cwd + "\Audio")
+        # cd "Audio"
         os.chdir(cwd + "\Audio")
-        #Change the current directory
+        # Change the current directory
         cwd = os.getcwd()
-    
+
     if(link.startswith("https://www.youtube.com/playlist?list=")):
         playlist = Playlist(link)
         print('Number of videos in playlist: %s' % len(playlist.video_urls))
@@ -75,32 +87,40 @@ def download():
             yt = YouTube(playlist.video_urls[i])
             ys = yt.streams.get_lowest_resolution()
             downloadVideos(ys)
+        print("Download End")
 
     elif(link.startswith("https://www.youtube.com/watch?v=")):
-        #Get youtube videos with highest resolution
+        # Get youtube videos with highest resolution
         yt = YouTube(link)
         ys = yt.streams.get_lowest_resolution()
         downloadVideos(ys)
+        print("Download End")
+
+    else:
+        print("Link not valid")
 
 i = 0
 
-#Definition for Tkinter
+# Definition for Tkinter
 root = Tk()
 root.geometry('380x380')
 root.title("Youtube Audio Download (write by Simone Zoppelletto)")
 root.configure(background='#4d0000')
 
-#URL input (YouTube)
-label_URL = Label(root, text="Link to the Youtube video", background='#4d0000', foreground="white", padx=10, pady=10).pack()
-URL = Entry(root, width = 50)
+# URL input (YouTube)
+label_URL = Label(root, text="Link to the Youtube video",
+                  background='#4d0000', foreground="white", padx=10, pady=10).pack()
+URL = Entry(root, width=50)
 URL.pack()
 
-#Like Padx, Pady but between Input Label and Button Label
-skipLine = Label(root, text="", background='#4d0000', foreground="white").pack()
+# Like Padx, Pady but between Input Label and Button Label
+skipLine = Label(root, text="", background='#4d0000',
+                 foreground="white").pack()
 
 Button(root, text="Input", command=download).pack()
 
-#Footer Credits
-footer = Label(root, text="Created by Simone Zoppelletto", background='#4d0000', foreground="white", padx=10, pady=10).pack()
+# Footer Credits
+footer = Label(root, text="Created by Simone Zoppelletto",
+               background='#4d0000', foreground="white", padx=10, pady=10).pack()
 
 root.mainloop()
